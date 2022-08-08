c5481e6d25855a2f1e05375b85a16cfd796a43c0
Hp <hossein.nk1992@gmail.com>
2022-08-09 01:47:14 +0430

    test

--- app/src/main/java/com/orbitsoft/teamorbitsoft/Nikkhu.java
+++ app/src/main/java/com/orbitsoft/teamorbitsoft/Nikkhu.java
@@ -3,12 +3,42 @@ package com.orbitsoft.teamorbitsoft;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Nikkhu extends AppCompatActivity {
+    TextView t1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nikkhu);
+        t1=findViewById(R.id.textView3);
+
+
+
+
+
+
+
+
+        for ( int i=1;i<31;i++){
+            if (i%2!=0)
+                Toast.makeText(getApplicationContext(), String.valueOf(i),Toast.LENGTH_LONG).show();
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
     }
 }
