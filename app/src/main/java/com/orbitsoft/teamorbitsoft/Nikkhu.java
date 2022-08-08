ceb7ba0010c13e0c5214144c2ba14daa6b8606f0
Hp <hossein.nk1992@gmail.com>
2022-08-09 04:15:12 +0430

    test1

--- app/src/main/java/com/orbitsoft/teamorbitsoft/Nikkhu.java
+++ app/src/main/java/com/orbitsoft/teamorbitsoft/Nikkhu.java
@@ -3,8 +3,12 @@ package com.orbitsoft.teamorbitsoft;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.Random;
 
 public class Nikkhu extends AppCompatActivity {
     TextView t1;
@@ -27,6 +31,22 @@ public class Nikkhu extends AppCompatActivity {
             if (i%2!=0)
                 Toast.makeText(getApplicationContext(), String.valueOf(i),Toast.LENGTH_LONG).show();
 
+            //**************************************
+            // barnameye hasele jame adade bozorgtar
+
+            Random myRand= new Random();
+            int[] myNumber =new int[5];
+
+            for(int k=0;k<5;k++)
+                myNumber[k]=myRand.nextInt(100);
+
+            Arrays.sort(myNumber);
+            int sum=myNumber[3]+myNumber[4];
+
+            for(int j=0;j<5;j++)
+                Log.d("msg1",String.valueOf(j));
+                Log.d("msg2",String.valueOf(sum));
+
 
 
 
@@ -41,4 +61,7 @@ public class Nikkhu extends AppCompatActivity {
 
 
     }
+
+
+
 }
