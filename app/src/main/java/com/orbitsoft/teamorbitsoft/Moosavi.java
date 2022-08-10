644c4db079623d420ba338330c04f9a0e65c15a8
richeous2kand <richeous2kand@gmail.com>
2022-08-10 14:52:41 +0430

    Shahriar Moosavi

--- app/src/main/java/com/orbitsoft/teamorbitsoft/Moosavi.java
+++ app/src/main/java/com/orbitsoft/teamorbitsoft/Moosavi.java
@@ -23,6 +23,7 @@ public class Moosavi extends AppCompatActivity {
 
 
 
+
         //----------------------------------------------------------------- محاسبه ی میانگین 4 عدد
 
         int resault ;
@@ -35,5 +36,51 @@ public class Moosavi extends AppCompatActivity {
         Log.d( " test 2 " , Integer.toString( resault ) );
 
 
+
+
+        // ------------------------------------------------------------------------ نمایش اعداد  فرد
+
+        for ( int i = 1 ; i <= 30 ; i++ )
+        {
+            if ( ( i % 2) =! 0 )
+            {
+                Log.d( " test 3 " , Integer.intoString(i)) ;
+            }
+        }
+
+
+
+
+        // ------------------------------------------------------------------------جدول ضرب 100 تایی
+
+        String table = "\n" ;
+        string a , b ;
+        for ( int i = 1 ; i <= 10 ; i++ )
+        {
+            for (int j = 1 ; j <= 10 ; j++ )
+            {
+                table = table + Integer.intoString( i * j ) + " " ;
+            }
+            table = table + "\n" ;
+        }
+        Log.d ( "test 4" , table ) ;
+
+
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
