f1b8bdb5a29d5564c33a994f979ac5c7d94e669f
omidedriss <omidedriss@DESKTOP-7OUEIQN>
2022-08-12 02:43:54 +0430

    add Package Salimi

---
+++ app/src/main/java/com/orbitsoft/teamorbitsoft/Salimi/SalimiActivity.java
@@ -0,0 +1,16 @@
+package com.orbitsoft.teamorbitsoft.Salimi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.orbitsoft.teamorbitsoft.R;
+
+public class SalimiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salimi);
+    }
+}
