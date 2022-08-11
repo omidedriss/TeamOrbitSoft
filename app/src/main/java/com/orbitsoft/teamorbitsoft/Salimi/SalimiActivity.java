5dd686966164580386fe19a82aae311975ca36f2
omidedriss <omidedriss@desktop-7oueiqn>
2022-08-12 02:43:54 +0430

    add Package Salimi

---
+++ app/src/main/java/com/orbitsoft/teamorbitsoft/Salimi/SalimiActivity.java
@@ -0,0 +1,28 @@
+package com.orbitsoft.teamorbitsoft.Salimi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.orbitsoft.teamorbitsoft.R;
+
+public class SalimiActivity extends AppCompatActivity {
+TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salimi);
+        text = findViewById( R.id.textView );
+
+        //جدول ضرب
+        String n ="";
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                n += "\t" + i + " * " + j + " = " + ( i * j );
+            }
+            text.setText(n);
+        }
+        //******
+    }
+}
