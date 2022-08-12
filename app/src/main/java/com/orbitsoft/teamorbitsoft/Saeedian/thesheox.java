1e02ce6f131ea1645cbcbca44f65586189b679b6
thesheox <thesheox@gmail.com>
2022-08-12 13:28:43 +0430

    a

---
+++ app/src/main/java/com/orbitsoft/teamorbitsoft/Saeedian/thesheox.java
@@ -0,0 +1,15 @@
+package com.orbitsoft.teamorbitsoft.Saeedian;
+
+public class thesheox {
+}
+
+    private String name,family;
+    int height,weight,id_number;
+
+    Public Void Start (){
+ ..
+    }
+    Public Void Turbo Cooling () {
+ ..
+    }
+}
