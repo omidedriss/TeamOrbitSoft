6bb3eeb4775033643cbd341d00785af00381b023
omidedriss <omidedriss@DESKTOP-7OUEIQN>
2022-08-13 14:58:01 +0430

    add Menu

--- app/src/main/java/com/orbitsoft/teamorbitsoft/example/MyDialog.java
+++ app/src/main/java/com/orbitsoft/teamorbitsoft/example/MyDialog.java
@@ -7,6 +7,7 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -35,47 +36,73 @@ public class MyDialog extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-
     @Override
-    protected Dialog onCreateDialog(int id) {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_settings:
+                AlertDialog();
+                return true;
+//            case R.id.help:
+//                showHelp();
+//                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+  //  @Override
+    public void AlertDialog() {
         // TODO Auto-generated method stub
 
-        switch(id){
 
-            case 0:
-                return new AlertDialog.Builder(this)
-                        .setIcon(R.drawable.ic_action_search)
-                        .setTitle("A Test Dialog")
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                 new AlertDialog.Builder(this)
+//                        .setIcon(R.drawable.ic_action_search)
+//                        .setTitle("A Test Dialog")
+//                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                // TODO Auto-generated method stub
+//                                Toast.makeText(getBaseContext(), "OK", Toast.LENGTH_LONG).show();
+//
+//                            }
+//                        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                Toast.makeText(getBaseContext(), "Cancel", Toast.LENGTH_LONG).show();
+//
+//                            }
+//                        }).setMultiChoiceItems(items, itemsChecked, new DialogInterface.OnMultiChoiceClickListener() {
+//                            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+//
+//                                Toast.makeText(getBaseContext(), items[which] + (isChecked ? "cheched !": "unchecked"), Toast.LENGTH_LONG).show();
+//
+//                            }
+//                        }).create();
+        new AlertDialog.Builder(MyDialog.this)
+                .setTitle("Delete entry")
+                .setMessage("Are you sure you want to delete this entry?")
 
-                            public void onClick(DialogInterface dialog, int which) {
-                                // TODO Auto-generated method stub
-                                Toast.makeText(getBaseContext(), "OK", Toast.LENGTH_LONG).show();
+                // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Continue with delete operation
+                    }
+                })
 
-                            }
-                        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                // A null listener allows the button to dismiss the dialog and take no further action.
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
 
-                            public void onClick(DialogInterface dialog, int which) {
-                                Toast.makeText(getBaseContext(), "Cancel", Toast.LENGTH_LONG).show();
 
-                            }
-                        }).setMultiChoiceItems(items, itemsChecked, new DialogInterface.OnMultiChoiceClickListener() {
-                            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
 
-                                Toast.makeText(getBaseContext(), items[which] + (isChecked ? "cheched !": "unchecked"), Toast.LENGTH_LONG).show();
-
-                            }
-                        }).create();
+//        }
 
 
 
 
-        }
-
-
-
-
-        return super.onCreateDialog(id);
+      //  return super.onCreateDialog(id);
     }
 
 
