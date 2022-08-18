35c3958e6a95e223dcb9f7b94d6c5be8289fe017
Abbas <absgorji@gmail.com>
2022-08-18 19:12:34 +0430

    عباس گرجی تکمیل قسمت دیالوگ

--- app/src/main/java/com/orbitsoft/teamorbitsoft/Gorji/Gorji.java
+++ app/src/main/java/com/orbitsoft/teamorbitsoft/Gorji/Gorji.java
@@ -7,12 +7,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.orbitsoft.teamorbitsoft.R;
+import com.orbitsoft.teamorbitsoft.example.MyDialog;
 
 public class Gorji extends AppCompatActivity implements View.OnClickListener {
 Button insta,myInfo,metaco,exit;
-
+int x ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +24,14 @@ Button insta,myInfo,metaco,exit;
         metaco=findViewById(R.id.metaco);
         exit=findViewById(R.id.exit);
         insta.setOnClickListener(this);
-        myInfo.setOnClickListener(this);
+        myInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent init = new Intent(Gorji.this, GorjiMyDialogActivity.class);
+                startActivity(init);
+
+            }
+        });
         metaco.setOnClickListener(this);
         exit.setOnClickListener(this);
 
@@ -41,13 +50,22 @@ Button insta,myInfo,metaco,exit;
 
     @Override
     public void onClick(View v) {
-        if(v . equals(insta.getId())){
+        if(v .equals(insta.getId())){
+            Toast.makeText(getApplicationContext(),"insta",Toast.LENGTH_LONG).show();
+            Uri uri = Uri.parse("https://instagram.com/abbas1gorji");
+            Intent init = new Intent(Intent.ACTION_VIEW,uri);
+            init.setPackage("com.instagram.android");
 
+        startActivity(init);
         }
-        if(v.equals(myInfo.getId())){
+       // if(v.equals(myInfo.getId())){
 
-        }
+        //}
         if(v.equals(metaco.getId())){
+            Intent init1 = new Intent(Intent.ACTION_VIEW);
+            Uri uri = Uri.parse("https://tr.metacomplex.com/");
+            init1.setData(uri);
+            startActivity(init1);
 
         }
         if(v.equals(exit.getId())){
