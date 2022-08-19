95d5502f0567fd440502429381db5e6c5cbd0bf3
thesheox <thesheox@gmail.com>
2022-08-19 20:25:38 +0430

    menu final

--- app/src/main/java/com/orbitsoft/teamorbitsoft/Saeidian/shayan.java
+++ app/src/main/java/com/orbitsoft/teamorbitsoft/Saeidian/shayan.java
@@ -15,7 +15,6 @@ import com.orbitsoft.teamorbitsoft.R;
 
 public class shayan extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
