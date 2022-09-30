5e27cb2c1b1ffdb342e13d394c8c091f7584bf23
Hp <hossein.nk1992@gmail.com>
2022-09-30 17:40:53 +0330

    fast save

--- app/src/main/java/com/orbitsoft/teamorbitsoft/Nikkhu/nickoo_login.java
+++ app/src/main/java/com/orbitsoft/teamorbitsoft/Nikkhu/nickoo_login.java
@@ -8,22 +8,47 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.appizona.yehiahd.fastsave.FastSave;
 import com.orbitsoft.teamorbitsoft.R;
 
 public class nickoo_login extends AppCompatActivity implements View.OnClickListener{
     private EditText ed_username,ed_password;
     private Button bt_login;
     private TextView tx_signup;
+    private String user,pass,phon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nickoo_login);
-
+        FastSave.init(this);
         ed_password = (EditText) findViewById(R.id.ed_password);
         ed_username = (EditText) findViewById(R.id.ed_username);
         tx_signup = (TextView) findViewById(R.id.tx_signup);
+        bt_login=(Button) findViewById(R.id.bt_login);
+        bt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                user=FastSave.getInstance().getString("myuser","");
+                pass=FastSave.getInstance().getString("pass","");
+                phon=FastSave.getInstance().getString("phon","");
+//                Toast.makeText(nickoo_login.this, user, Toast.LENGTH_LONG).show();
+//                Toast.makeText(nickoo_login.this, pass, Toast.LENGTH_LONG).show();
+
+                if((ed_password.getText().toString().equals(pass))&&
+                        (ed_username.getText().toString().equals(user)))
+                    Toast.makeText(nickoo_login.this,
+                            "You are loged in", Toast.LENGTH_SHORT).show();
+
+                else{
+                    Toast.makeText(getApplicationContext(),
+                            "Wrong User Name or Password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
@@ -33,9 +58,19 @@ public class nickoo_login extends AppCompatActivity implements View.OnClickListe
                 Intent i=new Intent(getApplicationContext(),nickoo_signUp.class);
                 startActivity(i);
                 break;
-
+//            case R.id.bt_login:
+//                Toast.makeText(this, "button1", Toast.LENGTH_LONG).show();
+//               mylogin();
+//                break;
 
         }
+
+    }
+
+    public void mylogin() {
+        Toast.makeText(this, "button", Toast.LENGTH_LONG).show();
+
+
 
     }
 }
--- app/src/main/java/com/orbitsoft/teamorbitsoft/Nikkhu/nickoo_signUp.java
+++ app/src/main/java/com/orbitsoft/teamorbitsoft/Nikkhu/nickoo_signUp.java
@@ -2,14 +2,51 @@ package com.orbitsoft.teamorbitsoft.Nikkhu;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.appizona.yehiahd.fastsave.FastSave;
 import com.orbitsoft.teamorbitsoft.R;
 
 public class nickoo_signUp extends AppCompatActivity {
-
+private EditText ed_username,ed_password,ed_phon;
+private Button bt_register;
+private String user,pass,phon;
+public String in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nickoo_sign_up);
+        FastSave.init(this);
+        inputInit();
+        bt_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                user=ed_username.getText().toString().trim();
+                pass=ed_password.getText().toString().trim();
+                phon=ed_phon.getText().toString().trim();
+                if(user.isEmpty()||pass.isEmpty()||phon.isEmpty())
+                    Toast.makeText(nickoo_signUp.this, "Fill all the requirements", Toast.LENGTH_SHORT).show();
+
+                else {
+                    String[] info= new String[]{user,pass,phon};
+                    FastSave.getInstance().saveString("myuser",user);
+                    FastSave.getInstance().saveString("pass",pass);
+                    FastSave.getInstance().saveString("phon",phon);
+                    Toast.makeText(nickoo_signUp.this, "user Name and Password Saved Successfully", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        });
+    }
+
+    private void inputInit() {
+        ed_password=(EditText) findViewById(R.id.ed_password);
+        ed_username=(EditText) findViewById(R.id.ed_username);
+        ed_phon=(EditText) findViewById(R.id.ed_phon);
+        bt_register=(Button) findViewById(R.id.bt_register);
+
     }
 }
