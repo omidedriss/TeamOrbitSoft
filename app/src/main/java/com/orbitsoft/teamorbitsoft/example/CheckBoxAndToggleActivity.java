f56603ad94664a89cdbd2ed56e4ec74537997fff
omidedriss <omidedriss@DESKTOP-7OUEIQN>
2022-10-21 19:56:13 +0330

    ProgressBar Add

--- app/src/main/java/com/orbitsoft/teamorbitsoft/example/CheckBoxAndToggleActivity.java
+++ app/src/main/java/com/orbitsoft/teamorbitsoft/example/CheckBoxAndToggleActivity.java
@@ -2,6 +2,7 @@ package com.orbitsoft.teamorbitsoft.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +30,21 @@ public class CheckBoxAndToggleActivity extends AppCompatActivity {
         simpleToggleButton1 = (ToggleButton) findViewById(R.id.simpleToggleButton1);
         simpleToggleButton2 = (ToggleButton) findViewById(R.id.simpleToggleButton2);
 
+        simpleToggleButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Drawable img = CheckBoxAndToggleActivity.this.getResources().getDrawable(R.drawable.ic_action_search);
+                simpleToggleButton1.setCompoundDrawablesWithIntrinsicBounds(img, null, null, null);
+//                simpleToggleButton2.setChecked(true);
+
+//                if (simpleToggleButton1.isChecked()){
+//                    simpleToggleButton1.setText("روشن");
+//                }else {
+//                    simpleToggleButton1.setText("خاموش");
+//                }
+            }
+        });
+
         b2.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -44,8 +60,10 @@ public class CheckBoxAndToggleActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), status, Toast.LENGTH_SHORT).show(); // display the current state of toggle button's
 
                 StringBuffer result = new StringBuffer();
-                result.append("Thanks : ").append(ch1.isChecked());
-                result.append("\nThanks: ").append(ch2.isChecked());
+                String sample= "test"+"test";
+                result.append("Thanks : ").append(ch1.isChecked());//"Thanks : true"
+                result.append("\nThanks: ").append(ch2.isChecked());//"Thanks : true Thanks: true"
+               // if (ch1.isChecked()==true)
                 Toast.makeText(CheckBoxAndToggleActivity.this, result.toString(),
                         Toast.LENGTH_LONG).show();
             }
